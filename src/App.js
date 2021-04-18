d773c43af8678a070f84aa94cad04be96bb02a06
Coderc <harsh@coderc.in>
2021-04-18 20:06:36 +0530

    Added Material UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-            </header>
+            <Button variant="contained" color="primary">
+                Primary
+            </Button>
+            <Button variant="outlined" color="secondary">
+                Primary
+            </Button>
         </div>
     );
 }
